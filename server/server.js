c94d45951d1c5e85e4a01a81726b209eafbb103a
Roberto Alonso Matus John <regular.greener.grass@hotmail.com>
2018-10-04 16:28:22 -0300

    add test GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
-    res.status(400).send('ID not valid');
+    res.status(404).send('ID not valid');
   }
 
   Todo.findById(id).then((todo) => {
